9e0a1d03704eeffc5558e9d9125a8814e06b1fdf
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-12-14 08:19:44 +0000

    Add options menu dialog elements

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
 import { Link, useOutletContext } from 'react-router-dom';
 import { DEFAULT_PROFILE_PICTURE } from '../../helpers/constants';
+import { useCloseDropdown } from '../../helpers/hooks';
 import { getFullNameFromDetails, getRelativeTimestamp } from '../../helpers/util';
 import postStyles from './css/post.module.css';
 
 export function Post({ post }) {
     const { user } = useOutletContext();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const buttonRef = useRef();
+    const menuRef = useRef();
+
+    useCloseDropdown(menuRef, buttonRef, setShowMenu);
+
+    useEffect(() => {
+        if (menuRef.current) menuRef.current.show();
+    }, [showMenu]);
 
     return (
         <article className={postStyles.post}>
@@ -15,11 +27,34 @@ export function Post({ post }) {
             />
 
             <div className={postStyles.contents}>
-                <Link to={`/${post.author.handle}`}>
-                    {user._id === post.author._id
-                        ? 'You'
-                        : getFullNameFromDetails(post.author.details)}
-                </Link>
+                <div className={postStyles.top}>
+                    <Link to={`/${post.author.handle}`}>
+                        {user._id === post.author._id
+                            ? 'You'
+                            : getFullNameFromDetails(post.author.details)}
+                    </Link>
+
+                    <div className={postStyles.menuWrapper}>
+                        <button
+                            onClick={() => setShowMenu(!showMenu)}
+                            className={postStyles.menuButton}
+                            ref={buttonRef}
+                        >
+                            <svg width="64px" height="64px" viewBox="0 0 16 16" fill="currentColor">
+                                <g>
+                                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
+                                </g>
+                            </svg>
+                        </button>
+
+                        {showMenu && (
+                            <dialog className={postStyles.menu} ref={menuRef}>
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </dialog>
+                        )}
+                    </div>
+                </div>
 
                 <p>{post.body}</p>
 
